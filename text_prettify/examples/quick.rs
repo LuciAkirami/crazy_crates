ed0edb93232ca37a121b987800a5d847f52838af
LuciAkirami <ajsubscriber001@gmail.com>
2023-10-14 01:14:54 +0530

    new-example

---
+++ text_prettify/examples/quick.rs
@@ -0,0 +1,7 @@
+/*quick is a terminal type application where you can run the file 
+a terminal pops up, where you can test different text graphics like
+colors, styles for quick prototypes
+*/  
+fn main() {
+
+}
